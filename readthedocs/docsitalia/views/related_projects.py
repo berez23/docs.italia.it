a9a5a8c172eeca9260d5807bf288c6c7aa9fc478
Filippo Morelli <filippo@20tab.com>
2020-01-16 19:31:42 +0100

    Fix #270 Fix linting issues

--- readthedocs/docsitalia/views/related_projects.py
+++ readthedocs/docsitalia/views/related_projects.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 """Define endpoints for the related projects section ("Documenti correlati")."""
 
-from django.db.models import Prefetch
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.renderers import JSONRenderer
-from rest_framework.response import Response
 
 from readthedocs.api.v2.permissions import APIPermission
 from readthedocs.projects.models import Project
@@ -13,6 +11,7 @@ from ..serializers import RelatedProjectsSerializer
 
 
 class RelatedProjectsView(RetrieveAPIView):
+
     """A view returning 3 sections of related projects using a given project slug."""
 
     lookup_field = 'slug'
