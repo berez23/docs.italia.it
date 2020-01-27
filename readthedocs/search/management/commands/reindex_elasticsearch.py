berez23/docs.italia.it

commit d7c5284cd3a35ff3418da6905527c34f12b80d4e

Block future use of reindex_elasticsearch (#478)

readthedocs/search/management/commands/reindex_elasticsearch.py
@@ -129,6 +129,10 @@ class Command(BaseCommand):
         `--model <app_label>.<model_name>` parameter.
         Otherwise, it will reindex all the models
         """
+        wiki_url = 'https://github.com/italia/docs.italia.it/wiki/Comandi-utili#use-di-reindex_elasticsearch'
+        raise RuntimeError(
+            'Don\'t use this command.\nRun "python manage.py search_index --rebuild" instead.\nSee %s' % wiki_url
+        )
         models = None
         if options['models']:
             models = [apps.get_model(model_name) for model_name in options['models']]

commit 9338872509b07c9bc78bc26dd332efd47a8cbdf3

Fix linting broken by #478

readthedocs/search/management/commands/reindex_elasticsearch.py
@@ -129,10 +129,14 @@ class Command(BaseCommand):
         `--model <app_label>.<model_name>` parameter.
         Otherwise, it will reindex all the models
         """
-        wiki_url = 'https://github.com/italia/docs.italia.it/wiki/Comandi-utili#use-di-reindex_elasticsearch'
+        wiki_url = 'https://github.com/italia/docs.italia.it/wiki/' \
+                   'Comandi-utili#use-di-reindex_elasticsearch'
         raise RuntimeError(
-            'Don\'t use this command.\nRun "python manage.py search_index --rebuild" instead.\nSee %s' % wiki_url
+            'Don\'t use this command.\n'
+            'Run "python manage.py search_index --rebuild" instead.\n'
+            'See %s' % wiki_url
         )
+        # pylint: disable=W0101,use-symbolic-message-instead
         models = None
         if options['models']:
             models = [apps.get_model(model_name) for model_name in options['models']]